264b1d1a4c299db005e6b17d304b2a365e8c9c9e
gonviegas <gonv@tuta.io>
2023-09-13 00:43:47 +0100

    3.9 - 3.11

--- part3/phonebook-backend/index.js
+++ part3/phonebook-backend/index.js
@@ -5,6 +5,7 @@ const cors = require('cors')
 const app = express()
 app.use(express.json())
 app.use(cors())
+app.use(express.static('dist'))
 
 morgan.token('body', req => JSON.stringify(req.body))
 
