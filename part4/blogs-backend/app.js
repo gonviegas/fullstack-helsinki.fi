dd8250943f1edb8f4bbe8550b51be52a60ea35e9
gonviegas <gonv@tuta.io>
2023-10-02 16:25:12 +0100

    4.15

--- part4/blogs-backend/app.js
+++ part4/blogs-backend/app.js
@@ -3,9 +3,11 @@ const express = require('express')
 const app = express()
 const cors = require('cors')
 const blogsRouter = require('./controllers/blogs')
+const usersRouter = require('./controllers/users')
 const middleware = require('./utils/middleware')
 const logger = require('./utils/logger')
 const mongoose = require('mongoose')
+
 
 mongoose.set('strictQuery', false)
 
@@ -26,6 +28,7 @@ app.use(express.json())
 app.use(middleware.requestLogger)
 
 app.use('/api/blogs', blogsRouter)
+app.use('/api/users', usersRouter)
 
 app.use(middleware.unknownEndpoint)
 app.use(middleware.errorHandler)
