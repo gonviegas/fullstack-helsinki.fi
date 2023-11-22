51e282ef957562a8ce3efe36d427d44a5afc2c93
gonviegas <gonv@tuta.io>
2023-11-22 15:17:29 +0000

    8.4

--- part8/library-backend/index.js
+++ part8/library-backend/index.js
@@ -112,7 +112,7 @@ const typeDefs = `
   type Query {
     bookCount: Int!,
     authorCount: Int!,
-    allBooks: [Book!]!
+    allBooks(author: String): [Book!]!
     allAuthors: [Author!]!
   }
 `
@@ -121,7 +121,10 @@ const resolvers = {
   Query: {
     bookCount: () => books.length,
     authorCount: () => authors.length,
-    allBooks: () => books,
+    allBooks: (root, args) => {
+      if (args.author) return books.filter(b => b.author === args.author)
+      else return books
+    },
     allAuthors: () => authors
   },
   Author: {
