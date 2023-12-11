709ad72c1bb6210b20e8c4e0dbd182cb338c946f
gonviegas <gonv@tuta.io>
2023-12-11 00:28:24 +0000

    9.22

--- part9/patientor/backend/src/types.ts
+++ part9/patientor/backend/src/types.ts
@@ -1,8 +1,6 @@
-export enum Gender {
-  Male = 'male',
-  Female = 'female',
-  Other = 'other'
-}
+type UnionOmit<T, K extends string | number | symbol> = T extends unknown
+  ? Omit<T, K>
+  : never;
 
 export interface Diagnosis {
   code: string;
@@ -10,7 +8,53 @@ export interface Diagnosis {
   latin?: string;
 }
 
-export interface Entry {}
+export enum Gender {
+  Male = 'male',
+  Female = 'female',
+  Other = 'other'
+}
+
+export enum HealthCheckRating {
+  'Healthy' = 0,
+  'LowRisk' = 1,
+  'HighRisk' = 2,
+  'CriticalRisk' = 3
+}
+
+interface BaseEntry {
+  id: string;
+  description: string;
+  date: string;
+  specialist: string;
+  diagnosisCodes?: Array<Diagnosis['code']>;
+}
+
+interface HealthCheckEntry extends BaseEntry {
+  type: 'HealthCheck';
+  healthCheckRating: HealthCheckRating;
+}
+
+interface HospitalEntry extends BaseEntry {
+  type: 'Hospital';
+  discharge: {
+    date: string;
+    criteria: string;
+  };
+}
+
+interface OccupationalHealthcareEntry extends BaseEntry {
+  type: 'OccupationalHealthcare';
+  employerName: string;
+  sickLeave?: {
+    startDate: string;
+    endDate: string;
+  };
+}
+
+export type Entry =
+  | HospitalEntry
+  | OccupationalHealthcareEntry
+  | HealthCheckEntry;
 
 export interface Patient {
   id: string;
@@ -19,8 +63,9 @@ export interface Patient {
   ssn: string;
   gender: Gender;
   occupation: string;
-  entries: never[];
+  entries: Entry[];
 }
 
 export type NonSensitivePatient = Omit<Patient, 'ssn' | 'entries'>;
 export type NewPatient = Omit<Patient, 'id'>;
+export type EntryWithoutId = UnionOmit<Entry, 'id'>;
