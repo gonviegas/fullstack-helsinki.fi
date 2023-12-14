8f7ccb4470c3bf989c966616b72189d568bd6e47
gonviegas <gonv@tuta.io>
2023-12-14 11:16:26 +0000

    Fix submodule - copy folder without submodule

---
+++ part9/flight-diary/backend/src/index.ts
@@ -0,0 +1,20 @@
+import express from 'express';
+import cors from 'cors';
+
+const app = express();
+import diaryRouter from './routes/diaries';
+app.use(express.json());
+app.use(cors());
+
+const PORT = 3000;
+
+app.get('/ping', (_req, res) => {
+  console.log('someone pinged here');
+  res.send('pong');
+});
+
+app.use('/api/diaries', diaryRouter);
+
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
